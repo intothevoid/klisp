dedcb3a957b20663dd25d135128e2e45e85cb025
Karan Kadam <karank@outlook.com>
2018-08-25 19:01:31 +0930

    Added evaluation functions to evaluate polish notation

--- klisp.c
+++ klisp.c
@@ -6,9 +6,39 @@
 /* Declare a buffer for user input of size 2048 */
 static char input[BUFF_SIZE];
 
+long eval_op(long x, char* op, long y)
+{
+  if(!strcmp(op, "+")) { return x + y; }
+  if(!strcmp(op, "-")) { return x - y; }
+  if(!strcmp(op, "*")) { return x * y; }
+  if(!strcmp(op, "/")) { return x / y; }
+
+  return 0;
+}
+
+long eval(mpc_ast_t* t)
+{
+  if(strstr(t->tag, "number"))
+    return atoi(t->contents);
+
+  // Operator is always the second child
+  char* op = t->children[1]->contents;
+
+  // Store the 3rd child
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr"))
+  {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char** argv) 
 {
-
   // Declare our grammar for MPC
   mpc_parser_t* Number = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
@@ -46,6 +76,15 @@ int main(int argc, char** argv)
     {
       // Print the AST
       mpc_ast_print(r.output);
+
+      mpc_ast_t* tree = r.output;
+
+      // Evaluate the AST
+      long result = eval(tree);
+
+      // Print result
+      printf("\nResult: %ld\n", result);
+
       mpc_ast_delete(r.output);
     }
     else
